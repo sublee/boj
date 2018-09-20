7d1a0a31d9fd117d4714b88ea87da37b1cdb6bf0
Heungsub Lee <sub@subl.ee>
2018-09-21 02:05:38 +0900

    Solve 1012 again with BFS

--- 1012/Main.cs
+++ 1012/Main.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections.Generic;
 
 namespace Acmicpc
 {
+    using Vector2 = Tuple<int, int>;
+
     class Acmicpc1012
     {
         static void Main(string[] args)
@@ -15,79 +18,61 @@ namespace Acmicpc
                 // 배추를 심은 배추밭의 가로길이 M(1 ≤ M ≤ 50)과 세로길이 N(1 ≤ N ≤ 50),
                 // 그리고 배추가 심어져 있는 위치의 개수 K(1 ≤ K ≤ 2500)이 주어진다.
                 var parts = Console.ReadLine().Split(new Char[] { ' ' }, 3);
-                var m = int.Parse(parts[0]);
-                var n = int.Parse(parts[1]);
                 var k = int.Parse(parts[2]);
 
                 // 그 다음 K줄에는 배추의 위치 X(0 ≤ X ≤ M-1), Y(0 ≤ Y ≤ N-1)가 주어진다.
-                int[,] a = new int[m, n];
+                var positions = new HashSet<Vector2>(k);
                 for (int j = 0; j < k; j++)
                 {
                     parts = Console.ReadLine().Split(new Char[] { ' ' }, 2);
                     var x = int.Parse(parts[0]);
                     var y = int.Parse(parts[1]);
-                    a[x, y] = 1;
+                    positions.Add(new Vector2(x, y));
                 }
 
-                Console.WriteLine(Solve(a, m, n));
+                Console.WriteLine(Solve(positions));
             }
         }
 
-        static int Solve(int[,] a, int m, int n)
+        static int Solve(HashSet<Vector2> positions)
         {
-            int[,] b = new int[m, n];
-            bool[,] v = new bool[m, n];
+            var visited = new HashSet<Vector2>(positions.Count);
 
             int count = 0;
 
-            for (int i = 0; i < m; i++)
+            foreach (var position in positions)
             {
-                for (int j = 0; j < n; j++)
+                if (visited.Contains(position))
                 {
-                    if (v[i, j])
-                    {
-                        continue;
-                    }
-
-                    if (a[i, j] == 1)
-                    {
-                        Visit(a, m, n, i, j, v);
-                        count++;
-                    }
+                    continue;
                 }
+                BFS(position, positions, visited);
+                count++;
             }
 
             return count;
         }
 
-        static void Visit(int[,] a, int m, int n, int i, int j, bool[,] v)
+        static void BFS(Vector2 position, HashSet<Vector2> positions, HashSet<Vector2> visited)
         {
-            if (v[i, j])
-            {
-                return;
-            }
+            var q = new Queue<Vector2>();
+            q.Enqueue(position);
 
-            v[i, j] = true;
+            while (q.Count != 0)
+            {
+                position = q.Dequeue();
 
-            if (a[i, j] != 1) {
-                return;
-            }
+                if (visited.Contains(position) || !positions.Contains(position))
+                {
+                    continue;
+                }
 
-            if (i > 0)
-            {
-                Visit(a, m, n, i-1, j, v);
-            }
-            if (i < m-1)
-            {
-                Visit(a, m, n, i+1, j, v);
-            }
-            if (j > 0)
-            {
-                Visit(a, m, n, i, j-1, v);
-            }
-            if (j < n-1)
-            {
-                Visit(a, m, n, i, j+1, v);
+                visited.Add(position);
+
+                q.Enqueue(new Vector2(position.Item1 + 1, position.Item2));
+                q.Enqueue(new Vector2(position.Item1 - 1, position.Item2));
+                q.Enqueue(new Vector2(position.Item1, position.Item2 + 1));
+                q.Enqueue(new Vector2(position.Item1, position.Item2 - 1));
             }
         }
     }
