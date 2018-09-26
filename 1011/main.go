b10ba64ad6eb469f800c7a1ec79d609b1846a4f6
Heungsub Lee <sub@subl.ee>
2018-09-26 20:55:35 +0900

    Solve 1011 by recursion

--- 1011/main.go
+++ 1011/main.go
@@ -30,5 +30,47 @@ func main() {
 }
 
 func solve(x, y int) int {
-	return 0
+	return _solve(y-x, 0)
+}
+
+const wrong = -1
+
+func min(a, b, c int) int {
+	min := a
+	if min == wrong || b != wrong && b < min {
+		min = b
+	}
+	if min == wrong || c != wrong && c < min {
+		min = c
+	}
+	return min
+}
+
+func _solve(d, k int) int {
+	if d <= 0 {
+		// too far
+		return wrong
+	}
+
+	if d == 1 {
+		// before the goal
+		if k <= 2 {
+			return 1
+		}
+
+		// too fast
+		return wrong
+	}
+
+	walk := func(k int) int {
+		if k <= 0 {
+			return wrong
+		}
+		return _solve(d-k, k)
+	}
+	a := walk(k)
+	b := walk(k + 1)
+	c := walk(k - 1)
+
+	return 1 + min(a, b, c)
 }
