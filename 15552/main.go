99a3dca53d8a10978beab3c7c2876951913bbb34
Heungsub Lee <sub@subl.ee>
2018-10-03 19:24:57 +0900

    Use bufio.Writer instead of bytes.Buffer

--- 15552/main.go
+++ 15552/main.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"os"
 )
 
 func main() {
 	r := bufio.NewReader(os.Stdin)
+	w := bufio.NewWriter(os.Stdout)
 
 	var t int
 	fmt.Fscanln(r, &t)
 
 	var a, b int
-	var buf bytes.Buffer
-
 	for i := 0; i < t; i++ {
 		fmt.Fscanln(r, &a, &b)
-		fmt.Fprintln(&buf, a+b)
+		fmt.Fprintln(w, a+b)
 	}
 
-	fmt.Print(buf.String())
+	w.Flush()
 }
