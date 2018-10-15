05d52c5e1e859704098941356e24837f25a8bdb1
Heungsub Lee <sub@subl.ee>
2018-10-16 01:50:16 +0900

    Solve 2581 in Go

---
+++ 2581 소수/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	var m, n int
+	fmt.Scan(&m, &n)
+
+	// Sieve of Eratosthenes
+	composites := make([]bool, n+1)
+	composites[1] = true
+
+	for i := 2; i <= n; i++ {
+		if !composites[i] {
+			for j := i * 2; j <= n; j += i {
+				composites[j] = true
+			}
+		}
+	}
+
+	// Calculate the prime sum and find the first prime.
+	var sum, first int
+
+	for i := m; i <= n; i++ {
+		if !composites[i] {
+			sum += i
+
+			if first == 0 {
+				first = i
+			}
+		}
+	}
+
+	if sum != 0 {
+		fmt.Println(sum)
+		fmt.Println(first)
+	} else {
+		fmt.Println(-1)
+	}
+}
