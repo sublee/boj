a69848e04ced14d7cac7dd06ecf58eecc60cad0d
Heungsub Lee <sub@subl.ee>
2018-11-15 15:49:11 +0900

    Solve 1022

--- 1022 소용돌이 예쁘게 출력하기/main.go
+++ 1022 소용돌이 예쁘게 출력하기/main.go
@@ -28,40 +28,48 @@ func max(a, b int) int {
 	return b
 }
 
-func spiral(radius int) [][]int {
-	distance := 1 + 2*radius
+func spiral(r1, c1, r2, c2 int) [][]int {
+	radius := max(abs(r1), max(abs(c1), max(abs(r2), abs(c2))))
 
 	// Init a spiral table.
-	tab := make([][]int, distance)
-	for i := 0; i < distance; i++ {
-		tab[i] = make([]int, distance)
+	tab := make([][]int, r2-r1+1)
+	for i := 0; i < len(tab); i++ {
+		tab[i] = make([]int, c2-c1+1)
 	}
 
-	dirs := [][2]int{{0, 1}, {-1, 0}, {0, -1}, {1, 0}}
+	// Anticlockwise
+	var (
+		right = [2]int{0, 1}
+		up    = [2]int{-1, 0}
+		left  = [2]int{0, -1}
+		down  = [2]int{1, 0}
+	)
+	dirs := [][2]int{right, up, left, down}
 
 	n := 1
 	k := 0
-	i, j := radius, radius
-	far, stay := 1, 1
+	i, j := 0, 0
+	far := 1
 
-	for i >= 0 && j >= 0 && i < distance && j < distance {
-		tab[i][j] = n
-		n++
-
+	distance := 1 + 2*radius
+	for n <= distance*distance {
 		d := dirs[k]
-		i += d[0]
-		j += d[1]
-		stay--
+
+		for l := 0; l < far; l++ {
+			if i >= r1 && i <= r2 && j >= c1 && j <= c2 {
+				tab[i-r1][j-c1] = n
+			}
+			n++
+
+			i += d[0]
+			j += d[1]
+		}
 
 		// Bounded.
-		if stay == 0 {
-			k = (k + 1) % len(dirs)
+		k = (k + 1) % len(dirs)
 
-			if k == 0 || k == 2 {
-				far++
-			}
-
-			stay = far
+		if k == 0 || k == 2 {
+			far++
 		}
 	}
 
@@ -69,38 +77,27 @@ func spiral(radius int) [][]int {
 }
 
 func solve(r1, c1, r2, c2 int) {
-	radius := max(abs(r1), max(abs(c1), max(abs(r2), abs(c2))))
-
 	// Make a spiral table.
-	tab := spiral(radius)
-
-	// Fix indices.
-	r1 += radius
-	c1 += radius
-	r2 += radius
-	c2 += radius
-
-	// Slice the table to contain numbers only in r1~r2/c1~c2.
-	view := make([][]int, r2-r1+1)
-	for i := 0; i < len(view); i++ {
-		view[i] = tab[r1+i][c1 : c2+1]
-	}
+	tab := spiral(r1, c1, r2, c2)
 
 	// Determine the printing format.
 	m := 0
-	for _, row := range view {
+	for _, row := range tab {
 		for _, col := range row {
 			m = max(m, col)
 		}
 	}
 
 	digits := int(math.Log10(float64(m))) + 1
-	format := "%" + strconv.Itoa(digits+1) + "d"
+	format := "%" + strconv.Itoa(digits) + "d"
 
 	// Print the view.
 	w := bufio.NewWriter(os.Stdout)
-	for _, row := range view {
-		for _, col := range row {
+	for _, row := range tab {
+		for j, col := range row {
+			if j > 0 {
+				fmt.Fprint(w, " ")
+			}
 			fmt.Fprintf(w, format, col)
 		}
 		fmt.Fprintln(w)
